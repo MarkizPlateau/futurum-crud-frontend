bf18ae1e9c1c7ca4180928fa23c777c6cf4896e7
Marek Adamczyk <s.marek.adamczyk@gmail.com>
2025-06-01 13:41:38 +0200

    style: add Lato font to global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import { Button } from "@chakra-ui/react";
-import { colorPalettes } from "compositions/lib/color-palettes";
 
 function App() {
   const [count, setCount] = useState(0);
