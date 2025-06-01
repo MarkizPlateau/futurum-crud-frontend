dea4a7f3e9332d5187cbf796e5d5b6838dd14ed2
Marek Adamczyk <s.marek.adamczyk@gmail.com>
2025-06-01 22:24:09 +0200

    feat: Add header, rename new campaign page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,100 @@
-import { Flex, Image, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Image,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import ColorModeSwitcher from "../ColorModeSwitcher/ColorModeSwitcher";
+import { useEffect, useState } from "react";
+import { CAMPAIGN_BUDGET } from "@/constants/constants";
+import { useColorMainText } from "@/hooks";
 
 export const Header = () => {
+  const [campaignBudget, setCampaignBudget] = useState<number>(5000);
+  const [currency, setCurrency] = useState<string>("PLN");
+
+  const boxShadowStrong = "0px 5px 15px 0px rgba(0, 0, 0, 0.4)";
+  const boxShadow = useColorModeValue("sm", boxShadowStrong);
+
+  useEffect(() => {
+    if (localStorage.getItem("campaigns")) {
+      const campaignsList = localStorage.getItem("campaigns");
+      if (campaignsList) {
+        const parsedCampaignList = JSON.parse(campaignsList);
+        const totalBudget = parsedCampaignList.reduce(
+          (acc: number, campaign: { fund: number }) =>
+            acc + Number(campaign.fund),
+          0
+        );
+
+        setCampaignBudget(campaignBudget - totalBudget);
+        localStorage.setItem("campaignsBudget", JSON.stringify(totalBudget));
+      }
+    } else {
+      const campaignBudget = localStorage.getItem("campaignsBudget");
+      if (campaignBudget) {
+        setCampaignBudget(JSON.parse(campaignBudget));
+      }
+    }
+  }, []);
+
   const { colorMode } = useColorMode();
   const isLightMode = colorMode === "light";
-  const headerBorderColor = useColorModeValue("main", "whiteAlpha.900");
+  const headerBorderColor = useColorModeValue("main", "customWhite");
+  const mainColorText = useColorMainText();
+
   return (
     <Flex
       as="header"
       borderBottomWidth="1px"
       borderBottomColor={headerBorderColor}
-      boxShadow="sm"
+      boxShadow={boxShadow}
       py={2}
     >
       <Flex
-        px={6}
+        px={{ base: "4", sm: "6" }}
         maxW="container.xl"
         w="full"
         mx="auto"
         align="center"
         justify="space-between"
       >
-        <a
-          href="https://futurumtechnology.pl/index.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src={isLightMode ? "/logo_light.png" : "/logo_dark.png"}
-            alt="Futurum Technology Logo"
-            height="70px"
-            objectFit="contain"
-          />
-        </a>
+        <Flex alignItems="center" gap={["2", "4"]}>
+          <a
+            href="https://futurumtechnology.pl/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit Futurum Technology website"
+          >
+            <Image
+              src={isLightMode ? "/logo_light.png" : "/logo_dark.png"}
+              alt="Futurum Technology Logo"
+              height={{ base: "50px", sm: "70px" }}
+              objectFit="contain"
+            />
+          </a>
+          <Heading
+            px="1"
+            as="h2"
+            fontSize={{ base: "sm", sm: "xl", md: "2xl" }}
+            color={mainColorText}
+          >
+            Total Budget{" "}
+            <Text mr="2" display={{ base: "none", md: "initial" }}>
+              of Campaign is
+            </Text>
+            <Text
+              as="span"
+              fontWeight="bold"
+              color={isLightMode ? "lightMode.orange" : "darkMode.orange"}
+            >
+              {campaignBudget ? campaignBudget.toFixed(2) : CAMPAIGN_BUDGET}{" "}
+              {currency}
+            </Text>
+          </Heading>
+        </Flex>
         <ColorModeSwitcher />
       </Flex>
     </Flex>
